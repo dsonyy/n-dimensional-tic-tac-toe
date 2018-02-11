8b7e725f920cbc6003180e297b7650f63a0602d4
dsonyy <dsonyyy@gmail.com>
2018-02-11 21:24:55 +0100

    looks like works on 3x3 map

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char ** argv)
 	VMapPos v(n);
 
 	//write(map);
-	set_field(map, O, 8);
-	check_win(map, 8);
+	set_field(map, O, 5);
+	check_win(map, 5);
 	for (int i = 0;; i = !i)
 	{
 
